6a4e63702e0524263b84764091f7b4a432ce7d30
dashamusic <158588894+dashamusic@users.noreply.github.com>
2024-02-29 10:12:46 +0530

    Update alive.py

--- Branded/plugins/alive.py
+++ Branded/plugins/alive.py
@@ -2,7 +2,7 @@ from .. import *
 
 @app.on_message(commandx(["alive"]))
 async def alive_check(client, message):
-    await message.reply_text("**🥀 𝗜 𝗔𝗠 𝗔𝗟𝗜𝗩𝗘 𝗠𝗬 𝗗𝗘𝗔𝗥 𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𝗞𝗜𝗡𝗚 𝗠𝗔𝗦𝗧𝗘𝗥✨ ...**")
+    await message.reply_text("**🥀 𝗜 𝗔𝗠 𝗔𝗟𝗜𝗩𝗘 𝗠𝗬 𝗗𝗘𝗔𝗥 ANONYMOUS 𝗠𝗔𝗦𝗧𝗘𝗥✨ ...**")
 
 
 
